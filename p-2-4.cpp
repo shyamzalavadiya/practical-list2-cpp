3d5b7c8c092a44ceda59c3aafe33aeeb8bc52204
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-09-28 08:28:43 +0530

    Update p-2-4.cpp

--- p-2-4.cpp
+++ p-2-4.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	// function setw & endl
 	int m = 10;
-	cout<<"Viradiya pinak"<<endl;
-	cout<<"220130318014"<<endl;
+	cout<<"zalavadiya shyam"<<endl;
+	cout<<"220130318015"<<endl;
 	cout<<"---------------------"<<endl;
 	cout<<"value of a is : "<<m<<endl;// Function endl 
 	cout<<"This is for function setw : "<<setw(5)<<setfill('0')<<m; // function setw 
