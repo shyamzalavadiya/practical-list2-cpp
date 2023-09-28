356515d73fb43c87a46210de78c0df5625227b37
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-09-28 08:29:54 +0530

    Update p-2-6.cpp

--- p-2-6.cpp
+++ p-2-6.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	// function setfill 
 	int a =10;
-	cout<<"Viradiya pinak"<<endl;
-	cout<<"220130318014"<<endl;
+	cout<<"zalavadiya shyam"<<endl;
+	cout<<"220130318015"<<endl;
 	cout<<"--------------------- "<<endl;
 	cout<<"The value of A :   "<<setw(5)<<setfill('*')<<a;
 	return 0;
